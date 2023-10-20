de20c8bd2d52e8c7565f12b993a61bf821daaa6a
rocket-racoon13 <navyhour.mj@gmail.com>
2023-10-21 00:56:11 +0900

    tokenizer updates

--- tokenizer.py
+++ tokenizer.py
@@ -1,54 +1,62 @@
+from collections import Counter, defaultdict
+import os
 import pickle
+from typing import List
+
 
 class Tokenizer:
-    def __init__(self):
-        with open("data/corpus.txt") as f_in:
-            self.corpus = f_in.read()
-        self.load_tokenizer("tokenizer.pkl")
+    def __init__(
+        self,
+        corpus_file_path="data/simple.txt",
+        top_k=1000,
+        mode="character"   # "character", "whitespace"
+    ):
+        self.top_k = top_k
+        self.mode = mode
+        if not os.path.exists(corpus_file_path):
+            raise IOError("=== Corpus file not found. ===")
+        else:
+            with open(f"{corpus_file_path}", encoding="utf-8-sig") as f_in:
+                self.corpus = f_in.read()
+        self.token_to_idx, self.idx_to_token = self.create_mapping_dict()
     
-    def preprocess(self, text):
-        return text.lower().split()
+    def tokenize(self, text):
+        text = text.lower()
+        if self.mode == "character":
+            all_tokens = [char for sequence in text.split() for char in sequence]
+        elif self.mode == "whitespace":
+            all_tokens = text.split()
+        return all_tokens
     
-    def create_tokenizer(self):
-        token_set = set(self.preprocess(self.corpus))
-        token_to_id = {token:id for id, token in enumerate(token_set, 1)}
-        token_to_id["<UNK>"] = 0
-        id_to_token = {token_to_id[token]:token for token in token_to_id}
-        dictionary = {
-            "token_to_id": token_to_id,
-            "id_to_token": id_to_token
-        }
-        with open("dictionary/tokenizer.pkl", "wb") as f:
-            pickle.dump(dictionary, f)
+    def create_mapping_dict(self):
+        token_to_idx, idx_to_token = {}, {}
+        token_counter = Counter(self.tokenize(self.corpus))
+        unique_tokens = list(token_counter.keys())[:self.top_k]
+        unique_tokens = ["EOS", "UNK"] + unique_tokens
+        for idx, token in enumerate(unique_tokens):
+            token_to_idx[token] = idx
+            idx_to_token[idx] = token
+        return token_to_idx, idx_to_token
     
-    def load_tokenizer(self, tokenizer_name):
-        with open(f"dictionary/{tokenizer_name}", "rb") as f:
-            dictionary = pickle.load(f)
-        self.token_to_id = dictionary["token_to_id"]
-        self.id_to_token = dictionary["id_to_token"]
+    def load_mapping_dict(self):
+        pass
     
-    def encode(self, input: list):
-        encoding = []
-        for token in self.preprocess(input[0]):
-            if self.token_to_id.get(token):
-                encoding.append(self.token_to_id.get(token))
-            else:
-                encoding.append(0)
-        return encoding
+    def encode(self, token):
+        return self.token_to_idx.get(token, self.token_to_idx["UNK"])
     
-    def decode(self, input: list):
-        decoding = []
-        for id in input:
-            if self.id_to_token.get(id):
-                decoding.append(self.id_to_token.get(id))
-            else:
-                decoding.append("<UNK>")
-        return decoding
+    def decode(self, idx):
+        return self.idx_to_token.get(idx, "UNK")
     
+    def encode_sequence(self, sequence: List[str]) -> List[int]:
+        return [self.encode(token) for token in sequence]
     
+    def decode_sequence(self, sequence: List[int]) -> List[str]:
+        return [self.decode(idx) for idx in sequence]
+
+
 if __name__ == "__main__":
     tokenizer = Tokenizer()
-    print(tokenizer.encode(["UNK"]))   # [0]
-    print(tokenizer.decode([0]))   # ['<UNK>']
-    print(tokenizer.encode(["I was in a hurry"]))  # [8845, 2302, 363, 2792, 5108]
-    print(tokenizer.decode([3, 5, 7, 9, 11]))  # ['stealthily', 'tool."', 'boldest,', 'scream', 'club']
+    print(tokenizer.encode("UNK"))   # 1
+    print(tokenizer.decode(0))   # "EOS"
+    print(tokenizer.encode_sequence(["a", "b", "a", "EOS"]))   # [2, 3, 2, 0]
+    print(tokenizer.decode_sequence([0, 1, 2, 3, 4, 5, 6]))   # ['EOS', 'UNK', 'a', 'b', 'UNK', 'UNK', 'UNK']
