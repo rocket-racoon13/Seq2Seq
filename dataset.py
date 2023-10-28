c90cd2537d0a4d851dc4c75f951d150093dd3d25
rocket-racoon13 <navyhour.mj@gmail.com>
2023-10-28 22:10:39 +0900

    dataset updates

--- dataset.py
+++ dataset.py
@@ -1,4 +1,7 @@
+from typing import Tuple, List
+
 import numpy as np
+import torch
 from torch.utils.data import Dataset
 
 
@@ -15,8 +18,36 @@ class CustomDataset(Dataset):
         return len(self.targets)
     
     def __getitem__(self, idx):
-        X = np.array(self.inputs[idx])
-        X = X.reshape(X.shape[0], X.shape[1], 1)
-        y = np.array(self.targets[idx])
-        y = y.reshape(y.shape[0], y.shape[1], 1)
+        # X -> Using LongTensor (indexing) for nn.Embedding
+        X = torch.LongTensor(self.inputs[idx])
+        y = torch.FloatTensor(self.targets[idx])
+        
         return X, y
+    
+    
+def custom_collator(batch: List[Tuple[torch.Tensor, torch.Tensor]]):
+    """
+    Pads variable length input to given max_len.
+    Collates list of input, target into a separate, stacked torch.Tensor.
+    """
+    inputs = [item[0] for item in batch]
+    targets = [item[1] for item in batch]
+    inputs = pad_sequences(inputs)   # variable length input padding
+    targets = torch.stack(targets, dim=0)
+    
+    
+def pad_sequences(sequences: List[torch.Tensor], max_len=None, padding="pre"):
+    batch_size = len(sequences)
+    if max_len is None:
+        longest_len = max([len(seq) for seq in sequences])
+    else:
+        longest_len = max_len
+    padded_seq = torch.zeros((batch_size, longest_len), dtype=torch.long)
+    
+    for i in range(batch_size):
+        seq = sequences[i]
+        if padding == "pre":
+            padded_seq[i,-len(seq):] = seq
+        elif padding == "post":
+            padded_seq[i,:len(seq)] = seq
+    return padded_seq
--- models/rnn.py
+++ models/rnn.py
@@ -18,19 +18,21 @@ class RNN(nn.Module):
         self.hidden_size = hidden_size
         self.embedding_size = embedding_size
         
-        self.embed_layer = nn.Embedding(self.input_size, self.embedding_size, 0)
+        self.encoder = nn.Embedding(self.input_size, self.embedding_size, 0)
         self.i2h = nn.Linear(self.embedding_size, self.hidden_size, bias=bias) # Stores W_ih & b_i
         self.h2h = nn.Linear(self.hidden_size, self.hidden_size, bias=bias) # Stores W_hh & b_h
         self.h2o = nn.Linear(self.hidden_size, self.input_size, bias=bias) # Stores W_ho & b_o
         
     def forward(self, x, h_prev):
-        embedding = self.embed_layer(x) # (batch_sz, seq_len, embed_sz)
+        print(x.shape)
+        embedding = self.encoder(x)   # (batch_sz, input_sz, embedding_sz)
+        print(embedding)
         print(embedding.shape)
-        weighted_input = self.i2h(embedding) # (batch_sz, input_sz, hidden_sz)
-        hidden_state = self.h2h(h_prev) # (hidden_sz, input_sz)
-        print(hidden_state.shape, h_prev.shape)
-        hidden_state = torch.tanh(weighted_input + hidden_state) # (batch_sz, hidden
-        output = self.h2o(hidden_state) # (batch_sz, hidden_sz,
+        weighted_input = self.i2h(embedding)   # (batch_sz, hidden_sz)
+        hidden_state = self.h2h(h_prev)   # (batch_sz, hidden_sz)
+        print(weighted_input.shape, hidden_state.shape)
+        hidden_state = torch.tanh(weighted_input + hidden_state)   # (batch_sz, hidden_sz)
+        output = self.h2o(hidden_state)   # (batch_sz, input_sz)
         return output, hidden_state
     
     def initialize_h_0(self, batch_size):
@@ -38,12 +40,14 @@ class RNN(nn.Module):
 
 
 if __name__ == "__main__":
-    seq_len = 4
     batch_size = 2
     vocab_size = 5
     hidden_size = 3
     embed_size = 6
     h_prev = torch.zeros(hidden_size)
-    a = torch.randint(0, 4, (batch_size, seq_len)) # (2, 4)
-    print(a)
-    print(a[:,0])
+    a = [[1, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0]]
+    a = torch.LongTensor(a)
+    rnn = RNN(vocab_size, hidden_size, embed_size)
+    output, hidden_state = rnn(a, rnn.initialize_h_0(batch_size))
+    print(output.shape, hidden_state.shape)
